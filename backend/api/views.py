46ed9a2afe662d5f7a7541503dd03fb6be487a5f
NOBODIDI <adrienmeryusa@gmail.com>
2025-03-19 15:41:22 -0400

    fixing bug in updateProfile

--- backend/api/views.py
+++ backend/api/views.py
@@ -24,6 +24,7 @@ import numpy as np
 import random
 
 from .algorithm.algorithm import cluster_and_match_students
+from django.db.models import Q
 
 @api_view(['GET'])
 def getStatus(request):
@@ -142,7 +143,7 @@ def getSelf(request):
 @permission_classes([IsAuthenticated])
 def updateProfile(request):
     if request.user.email:
-        user = get_user_model().objects.get(email__iexact(request.user.email))
+        user = get_user_model().objects.get(Q(email__iexact=request.user.email))
     elif request.user.username:
         user = get_user_model().objects.get(username=request.user.username)
     else:
